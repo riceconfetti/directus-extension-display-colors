9f44d4bf1893e14c3c42ae6b9ef0a9397aca1cd6
Rhylen Nguyen <rhylennguyen@gmail.com>
2025-03-01 12:28:17 -0500

    readd colors display

--- src/display.vue
+++ src/display.vue
@@ -12,13 +12,12 @@ let colors = JSON.parse(value);
 </script>
 
 <template>
-  <!-- <div class="flex gap-1 p-2 flex-wrap w-full h-full">
+  <div class="flex gap-1 p-2 flex-wrap w-full h-full">
     Value
     <span
       v-for="color in colors"
       class="rounded-full min-h-full size-4 flex bg-(--color-bg)"
       :style="{ '--color-bg': color.css }"
     />
-  </div> -->
-  <p>A Display</p>
+  </div>
 </template>
