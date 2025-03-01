553e8f8a090f27ee9674536a0615df7c7ec54cbc
Rhylen Nguyen <rhylennguyen@gmail.com>
2025-03-01 16:35:23 -0500

    remove parse

--- src/display.vue
+++ src/display.vue
@@ -7,21 +7,21 @@ export default {
       default: null,
     },
   },
-  data() {
-    let colors;
-    if (this.value != null && this.value != undefined) {
-      colors = JSON.parse(this.value);
-    } else {
-      colors = [];
-    }
-    return colors;
-  },
+  // data() {
+  //   let colors;
+  //   if (this.value != null && this.value != undefined) {
+  //     colors = JSON.parse(this.value);
+  //   } else {
+  //     colors = [];
+  //   }
+  //   return colors;
+  // },
 };
 </script>
 
 <template>
   <div class="flex gap-1 p-2 flex-wrap w-full h-full">
-    Value {{ value }} Colors {{ colors }}
+    Value {{ value }}
     <!-- <span
       v-for="color in colors"
       class="rounded-full min-h-full size-4 flex bg-(--color-bg)"
