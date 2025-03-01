bae60fff9f2d1a80444e1a57fa2461f6eb9f9e8f
Rhylen Nguyen <rhylennguyen@gmail.com>
2025-03-01 16:21:58 -0500

    fix?

--- src/display.vue
+++ src/display.vue
@@ -7,17 +7,26 @@ export default {
       default: null,
     },
   },
+  data() {
+    let colors;
+    if (value != null && value != undefined) {
+      colors = JSON.parse(this.value);
+    } else {
+      colors = [];
+    }
+    return colors;
+  },
 };
 </script>
 
 <template>
   <div class="flex gap-1 p-2 flex-wrap w-full h-full">
-    Value
-    <span
-      v-for="color in value"
+    Value {{ value }} Colors {{ colors }}
+    <!-- <span
+      v-for="color in colors"
       class="rounded-full min-h-full size-4 flex bg-(--color-bg)"
       :style="{ '--color-bg': color.css }"
-    />
+    /> -->
   </div>
 </template>
 
