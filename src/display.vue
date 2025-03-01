91b6da2190782389d1f24b511bd7876c6cbc9690
Rhylen Nguyen <rhylennguyen@gmail.com>
2025-03-01 11:55:48 -0500

    pushed changes

--- src/display.vue
+++ src/display.vue
@@ -1,16 +1,16 @@
 <script setup>
-import './app.css'
+import "./app.css";
 defineProps(["value"]);
 
-let colors = JSON.parse(value)
+let colors = JSON.parse(value);
 </script>
 
 <template>
   <div class="flex gap-1 p-2 flex-wrap w-full h-full">
     <span
       v-for="color in colors"
-      class="rounded-full min-h-full size-4 flex"
-      :style="'background:' + color.css"
+      class="rounded-full min-h-full size-4 flex bg-(--color-bg)"
+      :style="{ '--color-bg': color.css }"
     />
   </div>
 </template>
